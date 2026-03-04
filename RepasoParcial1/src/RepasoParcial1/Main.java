021a8c412d64f5b13a8bf09811b48605a53a05b8
Oscar <ocastillob3@miumg.edu.gt>
2026-03-03 23:30:27 -0600

    Estructura base del laboratorio

---
+++ RepasoParcial1/src/RepasoParcial1/Main.java
@@ -0,0 +1,61 @@
+package RepasoParcial1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int opcion;
+
+        do {
+            System.out.println("\n===== MENÚ PARCIAL 1 =====");
+            System.out.println("1. Suma divisibles entre 4");
+            System.out.println("2. Factorial");
+            System.out.println("3. Contar positivos, negativos y ceros");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ejercicio1();
+                    break;
+                case 2:
+                    ejercicio2();
+                    break;
+                case 3:
+                    ejercicio3();
+                    break;
+                case 0:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+
+        } while (opcion != 0);
+    }
+
+	private static void ejercicio3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ejercicio2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ejercicio1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+    // MÉTODOS AQUÍ ABAJO
+}
+	
+}
