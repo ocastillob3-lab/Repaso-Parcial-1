0f5d9c2f56151d6c7423ce48df9c63b0f9ea7923
Oscar <ocastillob3@miumg.edu.gt>
2026-03-05 20:08:20 -0600

    suma divisible

--- src/repasoparcial1/main.java
+++ src/repasoparcial1/main.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Scanner sc = new Scanner(System.in);
+	    static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+	    public static void main(String[] args) {
 
-      
+	    }
+
+	    // 1 suma divisible
+	    public static void ejercicio1() {
+
+	        System.out.print("Ingrese n: ");
+	        int n = sc.nextInt();
+
+	        int suma = 0;
+
+	        for (int i = 1; i <= n; i++) {
+	            if (i % 4 == 0) {
+	                suma += i;
+	            }
+	        }
+
+	        System.out.println("Suma divisibles entre 4: " + suma);
+
+	    }
+	}
