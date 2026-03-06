c7974785cdb2699a0e458cb371171a6a861555b7
Oscar <ocastillob3@miumg.edu.gt>
2026-03-05 23:06:32 -0600

    Promedio negativos

--- src/repasoparcial1/main.java
+++ src/repasoparcial1/main.java
@@ -300,4 +300,35 @@ public static void ejercicio2() {
      }
  }
  
+ // 15 Promedio negativos
+ public static void ejercicio15() {
+
+     System.out.print("Cantidad de numeros: ");
+     int n = sc.nextInt();
+
+     int suma = 0;
+     int contador = 0;
+
+     for (int i = 1; i <= n; i++) {
+
+         System.out.print("Numero: ");
+         int num = sc.nextInt();
+
+         if (num < 0) {
+
+             suma += num;
+             contador++;
+
+         }
+
+     }
+
+     if (contador > 0) {
+         double promedio = (double) suma / contador;
+         System.out.println("Promedio negativos: " + promedio);
+     } else {
+         System.out.println("No hay negativos");
+     }
+ }
+ 
     }
